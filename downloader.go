192844cf2b75349bb0b70b7649d4d90ad1b4656e
yeoldelaptop <idc@msn.com>
2024-11-22 17:36:17 +0900

    DownloadFile works, rough draft

--- downloader.go
+++ downloader.go
@@ -8,12 +8,22 @@ import (
 )
 
 func DownloadFile(stringURL string) error {
+	// Set up the client.
+	req, err := http.NewRequest("GET", stringURL, nil)
+	if err != nil {
+		return err
+	}
+	// Set the User-Agent header to mimic a browser
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0")
 	// Get the resource.
-	resp, err := http.Get(stringURL)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	//resp, err := http.Get(stringURL)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+	// should check resp code.
 	// Create file to save to.
 	fileName := path.Base(stringURL)
 	out, err := os.Create(fileName)
